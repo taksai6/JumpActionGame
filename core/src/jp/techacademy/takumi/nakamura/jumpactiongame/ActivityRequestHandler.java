482c4c3b67f6ae060a1bb0114985198e2e1736f0
taksai6 <ta93_5@yahoo.co.jp>
2018-05-01 22:00:45 +0900

    Initial commit

---
+++ core/src/jp/techacademy/takumi/nakamura/jumpactiongame/ActivityRequestHandler.java
@@ -0,0 +1,5 @@
+package jp.techacademy.takumi.nakamura.jumpactiongame;
+
+public interface ActivityRequestHandler {
+    public void showAds(boolean show);
+}
---
+++ core/src/jp/techacademy/takumi/nakamura/jumpactiongame/Enemy.java
@@ -0,0 +1,19 @@
+package jp.techacademy.takumi.nakamura.jumpactiongame;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Enemy extends Sprite {
+    // 横幅、高さ
+    public static final float ENEMY_WIDTH = 0.8f;
+    public static final float ENEMY_HEIGHT = 0.8f;
+
+
+    int mState;
+
+    public Enemy(Texture texture, int srcX, int srcY, int srcWidth, int srcHeight) {
+        super(texture, srcX, srcY, srcWidth, srcHeight);
+        setSize(ENEMY_WIDTH, ENEMY_HEIGHT);
+    }
+
+}
